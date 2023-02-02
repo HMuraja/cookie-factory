42ba111e440ca07d7e2589768e2aee94411ce30a
HMuraja <hilla.muraja@hotmail.com>
2023-02-02 17:40:45 +0000

    Add cookie recipes as constant variables

--- run.py
+++ run.py
@@ -12,8 +12,47 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('cookie_batches')
 
+"""The three available recipes are listed below as constant variables"""
+RASPBERRY_WHITECHOCOLATE_COOKIE = {
+    "wet ingredients": {
+        "Butter": 0.16,
+        "Vanilla Extract": 0.005,
+        "Raspberries": 0.10, 
+        "White Sugar": 0.16}, 
+    "dry ingredients": {
+        "Flour": 0.38,
+        "Baking Powder": 0.003,
+        "Baking Soda": 0.002,
+        "White Chocolate": 0.19}}
+
+CLASSIC_COOKIE = {
+    "wet ingredients": {
+        "Butter": 0.16,
+        "Vanilla Extract": 0.005,
+        "Egg Mix": 0.10,
+        "Light Brown Sugar": 0.16},
+    "dry ingredients": {
+        "Flour": 0.38,
+        "Baking Powder": 0.003,
+        "Baking Soda": 0.002,
+        "White Chocolate": 0.19}}
+
+PEANUT_BUTTER_COOKIE = {
+    "wet ingredients": {
+        "Butter": 0.18,
+        "Vanilla Extract": 0.005,
+        "Egg Mix": 0.11,
+        "Light Brown Sugar": 0.16},
+    "dry ingredients": {
+        "Flour": 0.30,
+        "Cacao Powder": 0.05,
+        "Baking Powder": 0.003,
+        "Baking Soda": 0.002,
+        "Reesee’s Chips": 0.19}}
+
+print(RASPBERRY_WHITECHOCOLATE_COOKIE)
+
 batches = SHEET.worksheet('batches')
 
-data = batches.get_all_values()
+batch_data = batches.get_all_values()
 
-print(data)
